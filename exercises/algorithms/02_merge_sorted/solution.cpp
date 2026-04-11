627051b186391a015a5f63a7be6cc07cc044ef34
skikklesman <rstewart555@gmail.com>
2026-04-11 10:49:37 -0400

    02_merge_sorted solved

--- exercises/algorithms/02_merge_sorted/solution.cpp
+++ exercises/algorithms/02_merge_sorted/solution.cpp
@@ -15,6 +15,46 @@
 //
 // Aim for O(n + m) using two pointers, not O((n+m) log(n+m)) via concat+sort.
 std::vector<int> merge_sorted(const std::vector<int>& a, const std::vector<int>& b) {
-    // TODO: implement
-    return {};
+    
+    std::vector<int> retVector;
+
+    size_t aSize = a.size();
+    size_t bSize = b.size();
+
+    int aIndex = 0;
+    int bIndex = 0;
+    while (aIndex < aSize && bIndex < bSize)
+    {
+        if (a[aIndex] > b[bIndex])
+        {
+            retVector.push_back(b[bIndex]);
+            bIndex++;
+        }
+        else
+        {
+            retVector.push_back(a[aIndex]);
+            aIndex++;
+        }
+    }
+
+    if (aIndex < aSize)
+    {
+        //If we got through all of the values in vector b, and there are still values in vector a, we assume that all of the values in b are greater,
+        // and add them to the return vector.
+        for (; aIndex < aSize; ++aIndex)
+        {
+            retVector.push_back(a[aIndex]);
+        }
+    }
+    else if (bIndex < bSize)
+    {
+        //If we got through all of the values in vector a, and there are still values in vector b, we assume that all of the values in b are greater,
+        // and add them to the return vector.
+        for (; bIndex < bSize; ++bIndex)
+        {
+            retVector.push_back(b[bIndex]);
+        }
+    }
+
+    return retVector;
 }
